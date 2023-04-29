180792330cbc4624f4bbfa59209675ddacc6d547
bdf007 <bdf007@gmail.com>
2023-04-29 15:55:20 +0200

    Completing a Level and Loading Next Level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public int lives, score;
+    [SerializeField] private int level;
     [SerializeField] private Player player;
     [SerializeField] private Transform playerSpawnPoint;
+    private Spawner[] spawners;
 
     public void LoseLife()
     {
@@ -37,5 +40,38 @@ public class GameManager : MonoBehaviour
         score += points;
         // update UI score
         // is the level complete?
+        CheckForLevelCompletion();
+    }
+
+    private void CheckForLevelCompletion()
+    {
+        if(!FindAnyObjectByType<Enemy>())
+        {
+            foreach(Spawner spawner in spawners)
+            {
+                if(!spawner.completed)
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    void CompletLevel()
+    {
+        // increase level
+        level++;
+        // check if there is a next level and load it
+        if (SceneManager.GetSceneAt(level) != null)
+        {
+            SceneManager.LoadScene(level);
+        }
+        else
+        {
+            // game over
+            Debug.Log("Game Won! Nice!");
+        }
+
+
     }
 }
